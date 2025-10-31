c9dadfcc0d399808f01032dd32ee576f0b137ac7
Pascal Braendli <pascal.braendli475@gmail.com>
2025-10-31 15:34:15 +0100

    chore(app): initialize React entry with Router and AuthProvider

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Protected from "./components/Protected";
+import { AuthProvider } from "./hooks/AuthProvider";   // ⬅️ import
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/login", element: <Login /> },
+      { path: "/", element: <Protected><Home /></Protected> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider>                                   {/* ⬅️ wrap hier */}
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
