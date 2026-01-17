a0b326a2f2fd9c99c5b2ac5a251baa1191738124
Anastasia Bologan <anastasia.bologan19@gmail.com>
2026-01-17 17:14:35 +0100

    update: new button name

--- app/src/components/AudioPlayer.tsx
+++ app/src/components/AudioPlayer.tsx
@@ -114,7 +114,7 @@ export function AudioPlayer({
               onClick={stopAudio}
               className={`${buttonBase} ${size}`}
             >
-              ⏹ Stopp
+              ⏹ Neu Start
             </button>
           )}
 
