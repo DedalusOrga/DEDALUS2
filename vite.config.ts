f30ba5e4d1ffa515b78e42326790d59e58981fb9
Philipp3857 <148771095+Philipp3857@users.noreply.github.com>
2026-01-25 22:57:41 +0100

    Änderung für Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/DEDALUS2/",
+  base: "/",
   plugins: [react()],
   server: { port: 5173, open: true },
   resolve: {
